d89544e8dc61f5b1a678e205368bc355ec466dc4
Gustavo <gustavo.santana@tooseguros.com.br>
2024-04-10 17:48:16 -0300

    enable commit signatures

--- git/setup.sh
+++ git/setup.sh
@@ -3,4 +3,10 @@ cat << EOF | sudo tee $(pwd)/git/.gitconfig
 [user]
     name = $GIT_USER
     email = $GIT_EMAIL
+[gpg "ssh"]
+	allowedSignersFile = ~/.config/git/allowed-signers
+[commit]
+	gpgsign = true
+[gpg]
+	format = ssh
 EOF
