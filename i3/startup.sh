8e55d9a4f1d216b2f3b9bf59dfd038572f151fe8
gustv000 <gucastiliao@gmail.com>
2025-07-05 19:40:43 -0300

    remove i3 workspaces from startup

--- i3/startup.sh
+++ i3/startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 i3-msg 'workspace 1; exec alacritty -e tmux new-session -A -s scratchpad;' && sleep 2 && \
-    i3-msg 'move scratchpad' && sleep 1 && \
-    i3-msg 'exec firefox; exec spotify-launcher; exec telegram-desktop; exec postman; exec alacritty -T tmux -e tmux'
+    i3-msg 'move scratchpad'
 
 i3-msg 'exec copyq; exec teamviewer'
